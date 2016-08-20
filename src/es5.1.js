52855e21fa80c816e80779a8d5a60bb247e634f2
knazark <knazark@gmail.com>
2016-08-20 19:54:55 +0300

    add es5.1

--- src/es5.1.js
+++ src/es5.1.js
@@ -30,65 +30,125 @@ var app = myApp.getInstance({
 
 app.getName();
 
-
-
-function Machine() {
-	var self = this;
-
-	self._enabled = false;
-
-	this.enable = function() {
-		self._enabled = true;
-	};
-
-	this.disable = function() {
-		self._enabled = false;
-	};
-}
-
-function CoffeeMachine(power) {
-	Machine.call(this);
-
-	var parentEnable = this.enable;
-	this.enable = function() {
-		parentEnable();
-		this.run();
-	};
-
-	this.run = function () {
-		// alert( this._enabled ); // true
-	}
-}
-
-
-
-var coffeeMachine = new CoffeeMachine(10000);
-console.log(coffeeMachine);
-coffeeMachine.enable();
+// function Machine() {
+// 	var self = this;
+//
+// 	self._enabled = false;
+//
+// 	this.enable = function() {
+// 		self._enabled = true;
+// 	};
+//
+// 	this.disable = function() {
+// 		self._enabled = false;
+// 	};
+// }
+//
+// function CoffeeMachine(power) {
+// 	Machine.call(this);
+//
+// 	var parentEnable = this.enable;
+// 	this.enable = function() {
+// 		parentEnable();
+// 		this.run();
+// 	};
+//
+// 	this.run = function () {
+// 		// alert( this._enabled ); // true
+// 	}
+// }
+//
+//
+//
+// var coffeeMachine = new CoffeeMachine(10000);
+// console.log(coffeeMachine);
+// coffeeMachine.enable();
 
 function Machines() {
-	this._enabled = false;
+	this.firstName = 'Kovtun';
 }
 
-Machines.prototype._enable = function() {
-	this._enabled = true;
+Machines.prototype.enable = function() {
 };
 
 Machines.prototype.disable = function() {
-	this._enabled = false;
 };
 
-var ma = new Machines();
+function coffeeMachines() {
 
-var coffeeMachines = Object.create(ma);
-
-coffeeMachines.enable = function () {
-	this._enable();
-	this._run();
 }
 
-coffeeMachines._run = function () {
-	alert(this._enabled);
+
+
+var tmpFunc = function () {};
+tmpFunc.prototype = Machines.prototype;
+coffeeMachines.prototype = new tmpFunc();
+
+coffeeMachines.prototype.enable = function () {
+	Machines.prototype.enable.apply(this, arguments);
+	this.run();
+};
+
+coffeeMachines.prototype.run = function () {
+	// alert(this._enabled);
+};
+
+//
+// coffeeMachines.prototype = new Machines();
+
+var coffee = new coffeeMachines();
+console.log(coffee);
+
+
+
+// function showList() {
+// 	alert( Array.prototype.join.call(arguments, " - ") );
+// }
+//
+// showList("Вася", "Паша", "Маша");
+
+String.prototype.repeat = function(times) {
+	// console.dir(new Array(10)
+	// 	.join().split(',')
+	// 	.map(function(item, index){ return ++index;}));
+	return new Array(times + 1).join(this);
+};
+
+console.log( "ля".repeat(3) );
+
+
+Object.prototype.each = function(f) {
+	for (var prop in this) {
+		var value = this[prop];
+		f.call(value, prop, value); // вызовет f(prop, value), this=value
+	}
 }
 
-console.log(coffeeMachines.enable());
+// Попробуем! (внимание, пока что это работает неверно!)
+// var user = {
+// 	name: 'Вася',
+// 	age: 25
+// };
+//
+// user.each(function(prop, val) {
+// 	alert( prop ); // name -> age -> (!) each
+// });
+
+
+Function.prototype.defer = function(ms) {
+	var f = this;
+	return function() {
+		var args = arguments,
+			context = this;
+		setTimeout(function() {
+			f.apply(context, args);
+		}, ms);
+	}
+}
+
+// проверка
+function f(a, b) {
+	alert( a - b );
+}
+
+f.defer(1000)(1, 2);
