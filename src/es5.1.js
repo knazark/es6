d47129e4a87e3b78e4d9a57234527dc758f0f8f1
knazark <knazark@gmail.com>
2016-08-28 18:16:43 +0300

    add es5.1

--- src/es5.1.js
+++ src/es5.1.js
@@ -30,6 +30,17 @@ var app = myApp.getInstance({
 
 app.getName();
 
+
+var obj = {
+	name: 'nazar',
+	sayName: function () {
+		console.log(this.name);
+	}
+}
+
+var f = obj.sayName.bind(obj);
+f();
+
 // function Machine() {
 // 	var self = this;
 //
@@ -147,8 +158,89 @@ Function.prototype.defer = function(ms) {
 }
 
 // проверка
-function f(a, b) {
-	alert( a - b );
+// function f(a, b) {
+// 	alert( a - b );
+// }
+//
+// f.defer(1000)(1, 2);
+
+
+function pow(x,n){
+	if(n != 1) {
+		return x * pow(x, n-1);
+	} else {
+		return x;
+	}
 }
 
-f.defer(1000)(1, 2);
+pow(2,3);
+
+function factorial(n){
+	console.log(n);
+	return n ? n * factorial(n-1) : 1;
+}
+
+console.log(factorial(5));
+
+var numbers = "[0, 1, 2, 3]";
+
+var numbers_json = JSON.parse(numbers);
+
+console.log(numbers);
+console.log(numbers_json);
+
+var user = '{ "name": "Вася", "age": 35, "isAdmin": false, "friends": [0,1,2,3] }';
+
+user = JSON.parse(user);
+console.log(user);
+
+// [1,2,3,4].forEach(function(i){
+// 	console.log(i);
+// });
+//
+// function asyncFunc(array) {
+// 	array.forEach(function(i){
+// 		(function(x) {
+// 			setTimeout(function() {
+// 				console.log(x);
+// 			}, 0);
+// 		})(i);
+// 	});
+// }
+//
+// asyncFunc([1,2,3,4]);
+
+
+// for (var i = 1; i <= 10; i++) {
+// 	(function (i) {
+// 		setTimeout(function () { console.log(i); }, 0);
+// 	})(i);
+// }
+
+
+
+var test1 = {
+	name : 'Hello'
+}
+
+var a = test1;
+console.log(a.name);
+
+var b = JSON.parse(JSON.stringify(a));
+b.name = 'pasha';
+
+console.log(a.name);
+
+// for(var i=0;i<10000;i++) document.write('<span></span>');
+// var elements=document.body.getElementsByTagName('span'); var len=elements.length;
+// var d=new Date;
+// for(var i=0;i<len;i++) elements[i];
+// alert("Последовательно:"+(new Date-d)+"мс");//(1) vard=newDate;
+// for(var i=0;i<len;i+=2) elements[i],elements[len-i-1];
+// alert("Вразнобой:"+(new Date-d)+"мс");
+
+// var elems=document.documentElement.childNodes;
+// console.log(elems);
+// [].forEach.call(elems, function(elem){
+// 	console.log(elem);
+// });
